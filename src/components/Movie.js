9b5f9110990568df3abf36e81aec2d07041e42cb
imkarthi16 <karthiveera@gmail.com>
2022-07-21 13:30:46 +0530

    react-http-requests

---
+++ src/components/Movie.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Movie.module.css";
+
+const Movie = (props) => {
+  return (
+    <li className={classes.movie}>
+      <h2>{props.title}</h2>
+      <h2>{props.releaseDate}</h2>
+      <p>{props.openingText}</p>
+    </li>
+  );
+};
+
+export default Movie;
